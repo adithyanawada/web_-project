c823b69edac6ae88cf73a083b7ef9882beaf08dd
adithyanawada <46069898+adithyanawada@users.noreply.github.com>
2018-12-22 19:44:19 +0530

    Add files via upload

---
+++ copy.php
@@ -0,0 +1,13 @@
+
+<?php
+$f1 = '/var/www/html/oldfile.txt';
+$f2 = '/var/www/html/newfile.txt';
+
+if (!copy($f1,$f2)) {
+   echo "File cannot be copied! ";
+}
+else {
+    echo "File has been copied!";
+}
+  echo fopen(newfile.txt);
+?>
